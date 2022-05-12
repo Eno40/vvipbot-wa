654f601e83f52c0dd6976cd9702d9ee92b892d5e
Eno40 <102885323+Eno40@users.noreply.github.com>
2022-05-12 21:41:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
 const BotName = 'VVIPINDO BOT'; 
 const instagram = 'agus_alnsyh71- kaelMk_'; 
-const aktif = 'Kapan bot aktif';
-const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+const aktif = 'Kapan bot aktif';24 jam
+const vcard = 'BEGIN:VCARD\n' // +6283175371513
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
+            + 'FN:Agus\n' // Eno Ar
+            + 'ORG:VVIPINDO;\n' // Igun
             + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
